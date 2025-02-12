ac2ea19ec3de3f6350cfa92964f5c23732c75fef
nathan-004 <148985551+nathan-004@users.noreply.github.com>
2025-02-12 20:41:11 +0100

    Delete script directory

--- script/login.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-            event.preventDefault(); // Empêche le formulaire de se soumettre normalement
-
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("password").value;
-
-});
--- script/register.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Empêche le formulaire de se soumettre normalement
-	
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
-	
-	console.log(username);
-	console.log(password);
-});
