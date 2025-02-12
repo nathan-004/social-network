9d1f03ffa2704d557fc662dd128298efda31f312
nathan-004 <148985551+nathan-004@users.noreply.github.com>
2025-02-12 20:39:56 +0100

    Add files via upload

---
+++ js/register.js
@@ -0,0 +1,26 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Empêche le formulaire de se soumettre normalement
+	
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+	
+    console.log(username);
+    console.log(password);
+
+    fetch("http://127.0.0.1:5000/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    })
+    .then(response => response.text()) // Convertir la réponse en texte
+    .then(data => {
+        data = data
+    })
+    .catch(error => console.error("Erreur :", error));
+});
+
