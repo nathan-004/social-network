166ef73f7b753c6e4a69900e0a37870a5ffc23a4
nathan-004 <148985551+nathan-004@users.noreply.github.com>
2025-02-13 09:37:22 +0100

    Update server.py

--- server/server.py
+++ server/server.py
@@ -3,10 +3,7 @@ from database import Database
 
 app = Flask(__name__)
 
-#------------------------------------------Database-----------------------------------
-def add_new_user(username, password):
-    pass
-
+db_profile = Database()
 
 @app.route('/register', methods=["POST", "OPTIONS"])
 def register():
@@ -22,12 +19,28 @@ def register():
     username = data.get("username")
     password = data.get("password")
 
-    response = add_new_user(username, password)
+    response = {"code": 0,
+        "message": "test"}
+
+    if not db_profile.user_exist(username):
+        try:
+            db_profile.add_user(username, password)
+            response["code"] = 0
+            response["message"] = "Utilisateur ajouté"
+        except:
+            response["code"] = 2
+            response["message"] = "Une erreur inattendue s'est produite"
+    else:
+        response["code"] = 1
+        response["message"] = "Nom d'utilisateur déjà utilisé"
+
+    response = jsonify(response)
+
     response.headers.add("Access-Control-Allow-Origin", "*")  # Autorise toutes les origines
     response.headers.add("Access-Control-Allow-Headers", "Content-Type")
     response.headers.add("Access-Control-Allow-Methods", "POST")
 
-    print(f"Utilisateur: {username}, Mot de passe: {password}")
+    print("Utilisateur: "+ username, "Mot de passe: " + password)
 
     return response
 
