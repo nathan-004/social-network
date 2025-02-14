08950fd37ea288de111205df9b4f27948973d7ed
nathan-004 <148985551+nathan-004@users.noreply.github.com>
2025-02-14 19:51:29 +0100

    Add files via upload

--- server/server.py
+++ server/server.py
@@ -25,7 +25,6 @@ def register():
     if not db_profile.user_exist(username):
         try:
             db_profile.add_user(username, password)
-            response["code"] = 0
             response["message"] = "Utilisateur ajouté"
         except:
             response["code"] = 2
@@ -34,15 +33,54 @@ def register():
         response["code"] = 1
         response["message"] = "Nom d'utilisateur déjà utilisé"
 
+    print("Utilisateur: "+ username, "Mot de passe: " + password, "Reponse: " + response["message"])
+
     response = jsonify(response)
 
     response.headers.add("Access-Control-Allow-Origin", "*")  # Autorise toutes les origines
     response.headers.add("Access-Control-Allow-Headers", "Content-Type")
     response.headers.add("Access-Control-Allow-Methods", "POST")
 
-    print("Utilisateur: "+ username, "Mot de passe: " + password)
-
     db_profile.close()
+
+    return response
+
+@app.route('/login', methods=["POST", "OPTIONS"])
+def login():
+    if request.method == 'OPTIONS':
+        # Répondre aux requêtes préflight (CORS)
+        response = jsonify({"message": "CORS OK"})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+        return response, 200
+
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    response = {"code": 0,
+        "message": "test"}
+    
+    db_profile = Database()
+
+    if db_profile.user_exist(username):
+        if db_profile.connect_account(username, password):
+            response["message"] = "Connexion réussie"
+        else:
+            response["code"] = 1
+            response["message"] = "Mot de passe incorrect"
+    else:
+        response["code"] = 2
+        response["message"] = "Nom d'utilisateur non-existant"
+
+    print("Utilisateur: "+ username, "Mot de passe: " + password, "Reponse: " + response["message"])
+
+    response = jsonify(response)
+
+    response.headers.add("Access-Control-Allow-Origin", "*")  # Autorise toutes les origines
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    response.headers.add("Access-Control-Allow-Methods", "POST")
 
     return response
 
