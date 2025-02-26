660f3de07bc4397d5991ce9a7186e16b902b573b
nathan-004 <148985551+nathan-004@users.noreply.github.com>
2025-02-26 16:59:32 +0100

    Add files via upload

--- server/server.py
+++ server/server.py
@@ -82,6 +82,80 @@ def login():
     response.headers.add("Access-Control-Allow-Headers", "Content-Type")
     response.headers.add("Access-Control-Allow-Methods", "POST")
 
+    db_profile.close()
+
+    return response
+
+@app.route('/addcontactrequest', methods=["POST", "OPTIONS"])
+def getuser():
+    if request.method == 'OPTIONS':
+        # Répondre aux requêtes préflight (CORS)
+        response = jsonify({"message": "CORS OK"})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+        return response, 200
+
+    data = request.get_json()
+    username = data.get("username")
+    contact_username = data.get("contact_username")
+
+    response = {"code": 0,
+        "message": "test"}
+    
+    db_profile = Database()
+
+    if db_profile.user_exist(contact_username):
+        try:
+            db_profile.add_contact_request(username, contact_username)
+            response["message"] = "Demande de contact envoyée"
+        except:
+            response["code"] = 2
+            response["message"] = "Erreur inattendue"
+    else:
+        response["code"] = 1
+        response["message"] = "L'utilisateur n'existe pas"
+
+    print("Utilisateur: "+ username, "Reponse: " + response["message"])
+
+    response = jsonify(response)
+
+    response.headers.add("Access-Control-Allow-Origin", "*")  # Autorise toutes les origines
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    response.headers.add("Access-Control-Allow-Methods", "POST")
+
+    return response
+
+@app.route('/getcontactrequests', methods=["POST", "OPTIONS"])
+def getcontactrequests():
+    if request.method == 'OPTIONS':
+        # Répondre aux requêtes préflight (CORS)
+        response = jsonify({"message": "CORS OK"})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+        return response, 200
+
+    data = request.get_json()
+    username = data.get("username")
+
+    response = {"code": 0,
+        "message": "test",
+        "data": []}
+    
+    db_profile = Database()
+
+    response["data"] = db_profile.get_data(username, "contacts_request_in")
+    response["message"] = "Succès"
+
+    print("Utilisateur: "+ username, "Reponse: " + response["message"], "Data:", response["data"])
+
+    response = jsonify(response)
+
+    response.headers.add("Access-Control-Allow-Origin", "*")  # Autorise toutes les origines
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    response.headers.add("Access-Control-Allow-Methods", "POST")
+
     return response
 
 if __name__ == "__main__":
