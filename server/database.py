811c0e2d03c58faa068724948c040e1d70a12667
nathan-004 <148985551+nathan-004@users.noreply.github.com>
2025-03-05 15:21:57 +0100

    Add files via upload

--- server/database.py
+++ server/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 import json
+import time
 
 class Database():
     def __init__(self, file="profiles.db"):
@@ -90,9 +91,12 @@ class Database():
 
         self.cursor.execute(f"SELECT {column} FROM users WHERE username=?", (username,))
 
-        json_contacts = self.cursor.fetchone()[0]
+        try:
+            json_contacts = self.cursor.fetchone()[0]
+        except TypeError:
+            return []
 
-        return json.loads(json_contacts)
+        return list(set(json.loads(json_contacts)))
 
     def add_contact(self, username, contact_username):
         """
@@ -115,6 +119,97 @@ class Database():
         Accept a contact request from request_username on the username profile
         """
 
+        contacts_requests = self.get_data(username, "contacts_request_in")
+        print(username, request_username)
+        
+        if request_username in contacts_requests:
+            self.add_contact(username, request_username)
+            self.add_contact(request_username, username)
+            a = contacts_requests.copy()
+            for name in a:
+                if name == request_username:
+                    contacts_requests.pop(contacts_requests.index(request_username))
+                    print(contacts_requests)
+            self.cursor.execute("UPDATE users SET contacts_request_in=? WHERE username=?", (json.dumps(contacts_requests), username))
+            self.conn.commit()
+
+            return 0
+        else:
+            return 1
+
+class Messages():
+    def __init__(self, file="profiles.db"):
+        self.conn = sqlite3.connect(file)
+        self.cursor = self.conn.cursor()
+        self.file = file
+        self.table_creation()
+
+    def table_creation(self):
+        """
+        Add a new table if it's not existing
+        """
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username1 TEXT NOT NULL,
+                username2 TEXT NOT NULL,
+                time int,
+                sender TEXT,
+                message TEXT)
+        """)
+
+        self.conn.commit()
+
+    def read_data(self):
+        self.cursor.execute("SELECT * FROM messages")
+
+        messages = self.cursor.fetchall()
+
+        for el in messages:
+            print(el)
+    
+    def new_message(self, users, sender, message):
+        """
+        Add a new message in the conversation
+
+        Parameters
+        ----------
+        users:list
+            Liste des utilisateurs de taille 2
+        sender:str
+            Nom de l'envoyeur
+        message:str
+            Contenu du message
+        """
+
+        users.sort() # Trier dans l'ordre alphabétique
+        user1, user2 = users[0], users[1]
+
+        self.cursor.execute("INSERT INTO messages (username1, username2, time, sender, message) VALUES (?, ?, ?, ?, ?)", (user1, user2, int(time.time()), sender, message))
+        self.conn.commit()
+
+    def get_messages(self, users):
+        """
+        Return a list of all the messages sorted by time
+
+        Parameters
+        ----------
+        users:list
+            List of the two users
+        """
+
+        users.sort()
+
+        self.cursor.execute("SELECT sender, time, message FROM messages WHERE username1=? AND username2=? ORDER BY time", (users[0], users[1]))
+        messages = self.cursor.fetchall()
+
+        return messages
+
 if __name__ == "__main__":
     db = Database()
+    db_messages = Messages()
     db.read_data()
+    db_messages.new_message(["user1", "user2"], "user2", "Ceci est un test")
+    db_messages.read_data()
+    db_messages.get_messages(["user1", "user2"])
--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from database import Database
+from database import Database, Messages
 
 app = Flask(__name__)
 
@@ -149,6 +149,160 @@ def getcontactrequests():
     response["message"] = "Succès"
 
     print("Utilisateur: "+ username, "Reponse: " + response["message"], "Data:", response["data"])
+
+    response = jsonify(response)
+
+    response.headers.add("Access-Control-Allow-Origin", "*")  # Autorise toutes les origines
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    response.headers.add("Access-Control-Allow-Methods", "POST")
+
+    return response
+
+@app.route('/acceptcontactrequest', methods=["POST", "OPTIONS"])
+def acceptcontactrequests():
+    if request.method == 'OPTIONS':
+        # Répondre aux requêtes préflight (CORS)
+        response = jsonify({"message": "CORS OK"})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+        return response, 200
+
+    data = request.get_json()
+    username = data.get("username")
+    contact_request = data.get("request_username")
+
+    response = {"code": 0,
+        "message": "test",
+        }
+    
+    db_profile = Database()
+
+    if db_profile.accept_contact_request(username, contact_request) == 0:
+        response["message"] = "Contact ajouté"
+    else:
+        response["code"] = 1
+        response["message"] = "Erreur innatendue"
+
+    print("Utilisateur: "+ username, "Reponse: " + response["message"])
+
+    response = jsonify(response)
+
+    response.headers.add("Access-Control-Allow-Origin", "*")  # Autorise toutes les origines
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    response.headers.add("Access-Control-Allow-Methods", "POST")
+
+    return response
+
+@app.route('/getcontacts', methods=["POST", "OPTIONS"])
+def getcontacts():
+    if request.method == 'OPTIONS':
+        # Répondre aux requêtes préflight (CORS)
+        response = jsonify({"message": "CORS OK"})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+        return response, 200
+
+    data = request.get_json()
+    username = data.get("username")
+
+    response = {"code": 0,
+        "message": "test",
+        "data": []
+        }
+    
+    db_profile = Database()
+
+    try:
+        response["data"] = db_profile.get_data(username)
+        response["message"] = "OK"
+    except:
+        response["code"] = 1
+        response["message"] = "Erreur innatendue"
+
+    print("Utilisateur: "+ username, "Reponse: " + response["message"], "Data:", response["data"])
+
+    response = jsonify(response)
+
+    response.headers.add("Access-Control-Allow-Origin", "*")  # Autorise toutes les origines
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    response.headers.add("Access-Control-Allow-Methods", "POST")
+
+    return response
+
+@app.route('/getmessages', methods=["POST", "OPTIONS"])
+def gemessages():
+    if request.method == 'OPTIONS':
+        # Répondre aux requêtes préflight (CORS)
+        response = jsonify({"message": "CORS OK"})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+        return response, 200
+
+    data = request.get_json()
+    username = data.get("username")
+    contact = data.get("contact")
+
+    response = {"code": 0,
+        "message": "test",
+        "data": []
+        }
+    
+    db_messages = Messages()
+
+    try:
+        response["data"] = db_messages.get_messages([username, contact])
+        response["message"] = "OK"
+    except:
+        response["code"] = 1
+        response["message"] = "Erreur innatendue"
+
+    print("Utilisateur: "+ username, "Reponse: " + response["message"], "Data:", response["data"])
+
+    response = jsonify(response)
+
+    response.headers.add("Access-Control-Allow-Origin", "*")  # Autorise toutes les origines
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    response.headers.add("Access-Control-Allow-Methods", "POST")
+
+    return response
+
+@app.route('/newmessage', methods=["POST", "OPTIONS"])
+def newmessage():
+    if request.method == 'OPTIONS':
+        # Répondre aux requêtes préflight (CORS)
+        response = jsonify({"message": "CORS OK"})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+        return response, 200
+
+    data = request.get_json()
+    sender = data.get("sender")
+    contact = data.get("contact")
+    message = data.get("message")
+
+    response = {"code": 0,
+        "message": "test"
+        }
+    
+    db_messages = Messages()
+    db_profile = Database()
+
+    try:
+        if db_profile.get_data(sender):
+            db_messages.new_message([sender, contact], sender, message)
+            response["message"] = "Message envoyé"
+        else:
+            response["code"] = 2
+            response["message"] = "Utilisateurs non contacts"
+    except:
+        response["code"] = 1
+        response["message"] = "Erreur innatendue"
+
+    print("Utilisateur: "+ sender, "Reponse: " + response["message"])
 
     response = jsonify(response)
 
